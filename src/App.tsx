1befa81d47e588ba73ad112b851ec25d17813a79
edsonyuri <edsonyuri2014@hotmail.com>
2026-01-22 16:07:52 -0300

    feat: remover itens e ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Activity, useState } from 'react'
+import { useState } from 'react'
 
 import iconSun from './assets/icons/icon-sun.svg'
 
@@ -24,12 +24,14 @@ export default function App() {
   })
 
   const addTask = () => {
-    setTaskList(prev => [...prev, { ...task, id: crypto.randomUUID() }])
+    if (task.task.trim()) setTaskList(prev => [...prev, { ...task, id: crypto.randomUUID() }])
   }
 
   const removeCompleteTask = () => {
     setTaskList(prev => prev.filter(e => e.completedStatus === false))
   }
+
+  const removeTask = (id: string) => setTaskList(prev => prev.filter(e => e.id !== id))
 
   const changeTaskStatus = (id: string) => {
     setTaskList(prev => prev.map(e => e.id === id ? { ...e, completedStatus: !e.completedStatus } : e))
@@ -54,7 +56,7 @@ export default function App() {
 
   return (
     <div className="flex flex-col">
-      <header className=' justify-between flex w-[100%] items-center mt-[10.5vh] mb-[6.2vh]' >
+      <header className=' justify-between flex w-[100%] items-center mt-[8.5vh] mb-[5.7vh]' >
         <h1 className='text-[2.6rem] tracking-[0.15em]'>TODO</h1>
         <div className="w-[25px]">
           <img src={iconSun} alt="" />
@@ -65,13 +67,14 @@ export default function App() {
         <div className="flex items-center h-[65px] bg-[hsl(235,24%,19%)] rounded-[5px] justify-start px-[25px] mb-[2.5vh] shadow-input border-0">
           <SelectionCircle />
 
-          <input className='text-[hsl(234,39%,85%)] text-[18px] h-full bg-transparent outline-none w-full'
+          <input className='text-[hsl(234,39%,85%)] text-[18px] h-full bg-transparent outline-none w-full caret-[hsl(220,98%,61%)]'
             name='task'
             onChange={e => handleTask(e)}
             type="text"
             id="taskInput"
             onKeyDown={e => handleKeyDown(e)}
             value={task.task}
+            placeholder='Create a new todo...'
           />
         </div>
 
@@ -79,12 +82,12 @@ export default function App() {
         <div className="bg-[hsl(235,24%,19%)] rounded-[5px] shadow-list">
           <ul id="todoList" className="list-none">
             {getTasksByFilter[currentFilter]().map(e => (
-              <ListElement key={e.id} id={e.id} changeTaskStatus={() => changeTaskStatus(e.id)} completed={e.completedStatus}>{e.task}</ListElement>
+              <ListElement key={e.id} id={e.id} changeTaskStatus={() => changeTaskStatus(e.id)} completed={e.completedStatus} removeTask={removeTask}>{e.task}</ListElement>
             ))}
           </ul>
 
           <div className="flex justify-between items-center px-[28px]">
-            <p className='text-[hsl(235,16%,43%)] text-[14px] font-semibold font-[500]'><span id="task_counter" className='text-[hsl(235,16%,43%)] text-[14px]'>{taskList.length}</span> items left</p>
+            <p className='text-[hsl(235,16%,43%)] text-[14px] font-semibold font-[400]'><span id="task_counter" className='text-[hsl(235,16%,43%)] text-[14px]'>{getTasksByFilter[currentFilter]().length}</span> items left</p>
 
             <div className="flex justify-around items-center gap-[20px] h-[56px] text-[hsl(233,14%,35%);]">
 
@@ -94,12 +97,12 @@ export default function App() {
 
             </div>
 
-            <button id="clear_completed" className='font-[500]' onClick={removeCompleteTask}>Clear Completed</button>
+            <button id="clear_completed" className='font-[400] text-[hsl(235,16%,43%)] text-[14px] hover:text-[hsl(236,33%,92%)]' onClick={removeCompleteTask}>Clear Completed</button>
           </div>
         </div>
 
         <footer className='h-[13vh] flex items-center justify-center'>
-          <p className='text-[hsl(235,16%,43%)] text-[14px] font-semibold font-[500]'>Drag and drop to recorder list</p>
+          <p className='text-[hsl(235,16%,43%)] text-[14px] font-semibold font-[400]'>Drag and drop to recorder list</p>
         </footer>
       </main>
 
--- src/components/ListElement.tsx
+++ src/components/ListElement.tsx
@@ -5,18 +5,19 @@ type ListElementProps = {
     children: string,
     id: string,
     changeTaskStatus: VoidFunction,
-    completed: boolean
+    completed: boolean,
+    removeTask: (id: string) => void
 }
 
-export default function ListElement({ children, id, changeTaskStatus, completed }: ListElementProps) {
+export default function ListElement({ children, id, changeTaskStatus, completed, removeTask }: ListElementProps) {
     return (
         <li className='flex items-center justify-between h-[65px] border-b border-[hsl(237,14%,26%)] px-[25px]' >
-            <input type="checkbox" id={id} className='hidden' onChange={changeTaskStatus} checked={completed}/>
+            <input type="checkbox" id={id} className='hidden' onChange={changeTaskStatus} checked={completed} />
             <label htmlFor={id} className='flex items-center'>
                 <SelectionCircle />
                 <span className='text-[18px] cursor-pointer'>{children}</span>
             </label>
-            <button>
+            <button onClick={() => removeTask(id)}>
                 <img src={iconCross} alt="" />
             </button>
 
